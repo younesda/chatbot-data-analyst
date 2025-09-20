6b61173a49bad51d82db6fd72e5dea58d5a41245
younesda <youneshachami9@gmail.com>
2025-09-20 19:32:56 +0000

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+#models.py
 from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean, ForeignKey, JSON, LargeBinary
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
