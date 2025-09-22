46f022697a2aec56597745e10fa384a72c282637
younesda <youneshachami9@gmail.com>
2025-09-22 09:32:44 +0000

    Update models.py

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String(255), unique=True, index=True, nullable=False)
     username = Column(String(100), unique=True, index=True, nullable=False)
-    hashed_password = Column(String(255), nullable=False)
+    hashed_password = Column(String(255), nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     
     # Relations
